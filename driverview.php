1a71cca57937ce6ff2e1293ab218dd5bfc8eb5c5
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2020-11-25 10:03:33 -0600

    initial

---
+++ driverview.php
@@ -0,0 +1,75 @@
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+<style>
+		table, th, td {
+  padding: 15px;
+  border: 1px solid black;
+	}
+
+ body{
+ 	margin:30px;
+}
+</style>
+<body>
+
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$db = 'project3';
+
+
+$conn = new mysqli($servernme, $username, $password, $db);
+?>
+
+<?php echo "<h1>Hello, <b>".$username.'</b></h1>';?>
+<h3>Your recent trips:</h3>
+<table style="width:80%">
+  <tr>
+    <th>Date</th>
+    <th>From City</th> 
+    <th>Dest Cities</th>
+  </tr>
+<?php
+$query = "select * from trips";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+ while($row = $result->fetch_assoc()) {
+ 	echo "
+  <tr>
+    <td>".$row['date']."</td>
+    <td>".$row['fromCity']."</td> 
+    <td>".$row['driverID']."</td> 
+  </tr>";
+  }
+}
+?>
+</table>
+
+<h3>Your trucks:</h3>
+<table style="width:20%">
+  <tr>
+    <th>TruckID</th>
+  </tr>
+<?php
+$query = "select truckID from drivers";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+ while($row = $result->fetch_assoc()) {
+ 	echo "
+  <tr>
+    <td>".$row['truckID']."</td>
+  </tr>";
+  }
+}
+?>
+</table>
+<div>
+	<h3>New trip</h3>
+	<form>
+	  <label for="fname">Start City</label><br>
+	  <input type="text" id="fname" name="fname"><br>
+	  <label for="lname">End cities</label><br>
+	  <input type="text" id="lname" name="lname">
+	</form>
+</div>
+</body>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$db = "project3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+?>
+
+<br>
+<a href='driverview.php'>driverview</a>
+<br>
+
+<table style="width:40%">
+  <tr>
+	<th>Zipcode</th>
+	<th>Name</th>
+	<th>Region</th>
+  </tr>
+<?php
+$query = 'select * from cities';
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>" . $row["zipcode"]. "</td><td>" . $row["cityName"]. "</td><td>" . $row["regionID"]. "</td></tr>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+</table>
